ce2942c9f030c8d51172e41c3c2d98ffd7e2b284
GabrielC51 <gabrielcmonteiro3@gmail.com>
2024-01-25 08:08:54 -0300

    atualização

---
+++ C++/swich.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num = 10;
+    switch(num){
+        case 9:
+            cout << "numero 9" << endl;
+            break;
+        case 10:
+            cout << "numero 9" << endl;
+            break;
+        case 11:
+            cout << "numero 9" << endl;
+            break;
+        default:
+            cout << "numero 9" << endl;
+            break;
+    }
+    while(true){
+
+    }
+    return 0;
+}
